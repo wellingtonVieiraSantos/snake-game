a539ad55ded4febc7dc376386c72050a3bf3089e
wellingtonVieiraSantos <wtvsantos@gmail.com>
2023-08-13 14:37:31 -0300

    bug fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,8 +51,6 @@ toggleMode.addEventListener('click',()=>{
     localStorage.setItem('color-mode', nameMode.value)
 })
 
-main.requestFullscreen()
-
 //creating the start event
 btn.addEventListener('click',() => {
     if(window.innerWidth < 767 || 
